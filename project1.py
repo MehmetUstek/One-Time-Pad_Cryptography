0d0925f0646bafe818ec1ca3d2519d5046b107f5
MehmetUstek <mustek17@ku.edu.tr>
2021-10-25 17:45:18 +0300

    Final Code

--- project1.py
+++ project1.py
@@ -8,10 +8,10 @@ l = int(L)
 plaintext_lst = []
 ciphertext_lst = []
 for i in range(n):
-    plaintext_lst.append((input("Plaintext "+str(i)+":\n")))
+    plaintext_lst.append((input("Plaintext "+str(i+1)+":\n")))
 
 for i in range(n):
-    ciphertext_lst.append((input("Ciphertext "+str(i)+":\n")))
+    ciphertext_lst.append((input("Ciphertext "+str(i+1)+":\n")))
 
 
 # For every plaintext and cipher pairs, iterate all over, until two keys match.
@@ -23,7 +23,7 @@ real_key = ""
 key_list1 = []
 key_list2 = []
 key_list3 = []
-# Need to have a key matrix?
+# Observed from the loops, the complexity is indeed O(N^2*L)
 for p in range(n):
     for p1 in range(n):
         for a in range(l):
@@ -53,6 +53,7 @@ for p in range(n):
     # print(key_list2)
 pair_list = []
 identical_keys = []
+# O(N^2) here for getting the identical keys.
 for j in range(n):
     if n>1:
         for k in range(n):
@@ -63,12 +64,16 @@ for j in range(n):
                 # pair_list.append((j,k))
 
 # print("identical_keys:\n", identical_keys)
+# Getting the identical keys from n=2, and applying it to a next possible key,
+# which will give the real key value.
 for j in range(n):
     if n>2:
         for i in range(len(identical_keys)):
             if identical_keys[i] == key_list3[j]:
                 real_key = identical_keys[i]
 
+# Now, we know the key, it is time to reveal every pair.
+# This is again O(N^2*L) which will yield a total of O(N^2*L) complexity.
 key_string = ""
 for p in range(n):
     for c in range(n):
@@ -91,18 +96,14 @@ for p in range(n):
 
 
 # Outputting pairs
-# print("pairsss", pair_list)
 list_of_pairs = []
 for l,m in pair_list:
     list_of_pairs.append(("Plaintext"+str(l+1), "Ciphertext"+ str(m+1)))
 
-# list_of_pairs.append("Plaintext"+)
 print("Pairs:")
 print(list_of_pairs)
 
-
-
-
+# Given data for test.
 # Plaintext 1: 5769736874686174496861646265656e626f726e6c6f6e676265666f7265
 # Plaintext 2: 4d7962726f7468657273676f746d657570616761696e737474686577616c
 # Plaintext 3: 4f6e656d6f7265646179616e644977696c6c62655468656b696e67466f72
@@ -110,17 +111,15 @@ print(list_of_pairs)
 # Ciphertext 2: d5f6b5fce745232fa3f112e95c6e65fdea3e1a8af3eb1d53fa280551e5c0
 # Ciphertext 3: cdf1a3f9fc5f273f8be012e35a4277fae43d0a81cbec165ff1230478f8d7
 
-# for p in range(n):
-#     obj = bytes.fromhex(plaintext_lst[i])
-#     ascii_string = obj.decode("ASCII")
-#     print(ascii_string)
-
-# key = binascii.b2a_hex(os.urandom(l))
-
-
 if n>2:
-    print("key:\n", key_string)
+    print("key:", key_string)
     print(real_key)
 else:
     print("One of these keys are correct but cannot differentiate with n<3:\n", key_string)
     print(identical_keys)
+
+# The output of the example in the assignment is as follows:
+# Pairs:
+# [('Plaintext1', 'Ciphertext3'), ('Plaintext2', 'Ciphertext1'), ('Plaintext3', 'Ciphertext2')]
+# key:
+# 9a98d0918837464bc288738738271294865278efa7837838934662178ab2
